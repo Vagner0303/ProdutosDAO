3b86c61e100c89c48ba1408703f1c2fb6405848b
Vagner Melo <vagnermelopires10@gmail.com>
2026-01-30 16:59:43 -0300

    Refactor ConnectionFactory2 with updated author info

--- config/ConnectionFactory2.java
+++ config/ConnectionFactory2.java
@@ -8,24 +8,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 /**
  *
- * @author Professor
+ * @author VAGNERMELOPIRES
  */
-
-// Responsabilidade: apenas conectar no banco
 public class ConnectionFactory2 {
-    // endereço do banco 
+    
     private static final String URL = "jdbc:mysql://localhost:3306/escoladao";
-    // Usuário do MySQL
+   
     private static final String USER = "root";
-    // Senha do MySQL 
+    
     private static final String PASS ="root";
-    // Método que devolve uma conexão pronta
+    
     public static Connection getConnection() throws SQLException{
-        // Abre a conexão e retorna para quem chamou 
+      
         return DriverManager.getConnection(URL,USER,PASS);
     }
     
+
 }
